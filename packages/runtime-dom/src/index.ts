333b6b1784cd7916fea34678739a28f45eb96ab5
liangsuhao <liangsuhao@egova.com.cn>
2024-09-08 19:16:53 +0800

    p29

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -2,3 +2,28 @@ import { patchProps } from './patchprop';
 import { nodeOps } from './nodeOps';
 
 export const renderOptionDom = {patchProps, ...nodeOps};
+
+export const createApp = (rootComponent, rootProps) => {
+    let app = createRender(renderOptionDom).createApp(rootComponent, rootProps);
+    let { mount } = app;
+    app.mount = function(container) {
+        container = nodeOps.querySelector(container);
+        container.innerHTML = '';
+
+        mount(container);
+    }
+    return app;
+}
+
+function createRender(renderOptionDom) {
+    return {
+        createApp:(rootComponent, rootProps) => {
+            let app = {
+                mount: (container) => {
+                    console.log(container, rootComponent, rootProps, renderOptionDom);
+                }
+            }
+            return app;
+        }
+    }
+}
