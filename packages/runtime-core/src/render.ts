7226a4a94ad4b213ee6d9c04784b6f2a00b70a21
liangsuhao <liangsuhao@egova.com.cn>
2024-09-18 21:26:49 +0800

    P46

--- packages/runtime-core/src/render.ts
+++ packages/runtime-core/src/render.ts
@@ -11,6 +11,7 @@ export function createRender(renderOptionDom) {
         remove: hostRemove,
         insert: hostIsert,
         createText: hostCreateTextElement,
+        setElementText: hostSetElementText,
     } = renderOptionDom;
     function setupRenderEffect(instance, container) {
         // 创建effect
@@ -57,7 +58,7 @@ export function createRender(renderOptionDom) {
         }
     }
 
-    const mountElement = (vnode, container) => {
+    const mountElement = (vnode, container, ancher=null) => {
         const {props, shapeFlag, type, children} = vnode;
         // 创建元素
         let el = vnode.el = hostCreateElement(type);
@@ -77,20 +78,115 @@ export function createRender(renderOptionDom) {
             }
         }
         // 插入对应的位置
-        hostIsert(el, container);
+        hostIsert(el, container, ancher);
     }
 
-    const processElement = (n1, n2, container) => {
+    const processElement = (n1, n2, container, ancher=null) => {
         if(n1 === null) {
-            mountElement(n2, container);
+            mountElement(n2, container, ancher);
         } else {
-            //更新
+            //更新，先比对属性
+            patchElement(n1, n2, container, ancher);
         }
     }
 
     const processText = (n1, n2, container) => {
         if(n1 === null) {
             hostIsert(n2.el=hostCreateTextElement(n2.children), container);
+        }
+    }
+
+    const patchElement = (n1, n2, container, ancher=null) => {
+        const el = (n2.el = n1.el);
+        const oldProps = n1.props || {};
+        const newProps = n2.props || {};
+        patchProps(el, oldProps, newProps);
+        patchChildren(n1, n2, el);
+    }
+
+    const patchProps = (el, oldProps, newProps) => {  // 比对props
+        // 1.旧的有，新的没有
+        // 2.新的有，旧的没有
+        // 3.新的旧的都有
+        if(oldProps !== newProps) {
+            for(let key in newProps) {
+                const prev = oldProps[key];
+                const next = newProps[key];
+                if(prev !== next) {
+                    hostPatchProp(el, key, prev, next);
+                }
+            }
+        }
+
+        for(let key in oldProps) {
+            if(!newProps[key]) {
+                hostPatchProp(el, key, oldProps[key], null);
+            }
+        }
+    }
+
+    const patchChildren = (n1, n2, el) => {
+        const c1 = n1.children;
+        const c2 = n2.children;
+        // 1.旧的有children，新的没有children 2.新的有children，旧的没有 3.都有且children都是文本 4.都有且children是数组
+        const pervShapeFlag = n1.shapeFlag;
+        const nextShapeFlag = n2.shapeFlag;
+        if(nextShapeFlag & shapeFlags.TEXT_CHILDREN) { //全是文本，直接进行替换
+            hostSetElementText(el, c2);
+        } else { // 不是文本，是数组
+            if(pervShapeFlag & shapeFlags.ARRAY_CHILDREN) { //都有且都是数组
+                patchKeyChildren(c1, c2, el);
+            } else { // 旧的是文本
+                // 删除文本，添加数组
+                hostSetElementText(el, '');
+                mountChildren(el, c2);
+            }
+        }
+    }
+
+    /**
+     * 儿子都是数组的情况
+     */
+    const patchKeyChildren = (c1, c2, el) => {
+        // vue2双指针  vue3
+        let i = 0 ;
+        let e1 = c1.length - 1;
+        let e2 = c2.length - 1;
+        // sync from start： 头部比对
+        while(i<=e1 && i<=e2) {
+            const n1 = c1[i];
+            const n2 = c2[i];
+            if(isSameVnode(n1, n2)) {
+                patch(n1, n2, el);
+            } else {
+                break;
+            }
+            i++;
+        }
+
+        while(i<=e1 && i<=e2) {
+            const n1 = c1[e1];
+            const n2 = c2[e2];
+            if(isSameVnode(n1, n2)) {
+                patch(n1, n2, el);
+            } else {
+                break;
+            }
+            e1--;
+            e2--;
+        }
+
+        if(i > e1) {
+            // 说明是节点变多，且要前追加
+            const nextProps = e2 + 1; //插入的位置
+            const ancher = nextProps < c2.length ? c2[nextProps].el : null; //判断插入的地方是头还是尾
+            while(i <= e2) {
+                patch(null, c2[i++], el, ancher)
+            }
+        } else if (i <= e1) { //说明节点变少
+            while(i <= e1) {
+                unmount(c1[i++]);
+            }
         }
     }
 
@@ -102,7 +198,7 @@ export function createRender(renderOptionDom) {
         hostRemove(vnode.el);
     }
 
-    const patch = (n1, n2, container) => {
+    const patch = (n1, n2, container, ancher = null) => {
         let { shapeFlag, type } = n2;
 
         if(n1 && !isSameVnode(n1, n2)) {
@@ -116,7 +212,7 @@ export function createRender(renderOptionDom) {
             } 
             default: {
                 if(shapeFlag & shapeFlags.ELEMENT) {
-                    processElement(n1, n2, container);
+                    processElement(n1, n2, container, ancher);
                 } else if(shapeFlag & shapeFlags.STATEFUL_COMPONENT) {
                     processComponent(n1, n2, container);
                 }
